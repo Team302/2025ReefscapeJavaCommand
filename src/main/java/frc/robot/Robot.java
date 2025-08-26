297115beed2d15a0dd23b2442a93850a01d5d976
notcharlie2026 <118229690+notcharlie2026@users.noreply.github.com>
2025-08-25 20:46:30 -0400

    Revert "Merge branch 'main' into add-Tale-cmd-groups"
    This reverts commit 8027223d72e6db0360e38fa9604ff6e304cef406, reversing
    changes made to 13b40a8b954fbb03890015b406a4189639215f53.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -54,6 +54,8 @@ public class Robot extends TimedRobot {
   @Override
   public void disabledExit() {}
 
+
+
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
@@ -99,6 +101,7 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
+
 
   @Override
   public void testExit() {}
